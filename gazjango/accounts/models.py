f72bcea38bd479c90d64ba931df5dd36d1123394
Dougal Sutherland <dougal@gmail.com>
2008-05-29 02:56:35 +0000

    added a few methods and some tests for position-related stuff

--- gazjango/accounts/models.py
+++ gazjango/accounts/models.py
@@ -1,18 +1,37 @@
 from django.db                  import models
 from django.contrib.auth.models import User
+from datetime                   import datetime
 
 class UserProfile(models.Model):
     "Extra information about users."
-    user      = models.ForeignKey(User, unique=True)
-    bio       = models.TextField(blank=True)
-    contact   = models.TextField(blank=True)
-    # many-to-many Articles
+    user    = models.ForeignKey(User, unique=True)
+    bio     = models.TextField(blank=True)
+    contact = models.TextField(blank=True)
+    # many-to-many Articles, has-many PositionsHeld
+    
+    def current_positions(self):
+        "Returns positions currently held by this user."
+        return self.positions.filter(date_end=None).all()
+    
+    def add_position(self, position, date_start=None, date_end=None):
+        "Adds a new PositionHeld relation for this user."
+        if date_start is None: date_start = datetime.today()
+        self.positions.add(PositionHeld.objects.create(
+            user_profile = self,       position    = position,
+            date_start   = date_start, date_end    = date_end))
+    
+    def __unicode__(self):
+        return self.user.username
+    
 
 
 class Position(models.Model):
     "A position in the organization: Staff Reporter, Editor-in-Chief, etc."
-    name       = models.CharField(blank=True, max_length=40)
-
+    name = models.CharField(blank=True, max_length=40)
+    
+    def __unicode__(self):
+        return self.name
+    
 
 class PositionHeld(models.Model):
     """A user's holding of a position.
@@ -21,6 +40,11 @@ class PositionHeld(models.Model):
     still held, date_end will be null."""
     
     user_profile = models.ForeignKey(UserProfile, related_name='positions')
-    position     = models.ForeignKey(Position)
+    position     = models.ForeignKey(Position, related_name='holdings')
     date_start   = models.DateField()
     date_end     = models.DateField(null=True, blank=True)
+    
+    def __unicode__(self):
+        return "%s (by %s)" % (self.position.__unicode__(),
+                               self.user_profile.__unicode__())
+    
---
+++ gazjango/accounts/tests.py
@@ -0,0 +1,35 @@
+import unittest
+from django.contrib.auth.models import User
+from accounts.models            import UserProfile, Position, PositionHeld
+from articles.models            import Article
+
+class UserTestCase(unittest.TestCase):
+    def setUp(self):
+        self.bob = User.objects.create(username="bob")
+        self.bob.userprofile_set.add(UserProfile())
+        
+        self.reader   = Position.objects.create(name="Reader")
+        self.reporter = Position.objects.create(name="Staff Reporter")
+        self.editor   = Position.objects.create(name="Arts Editor")
+    
+    def tearDown(self):
+        for x in (self.bob, self.reader, self.reporter, self.editor):
+            x.delete()
+    
+    def test_positions_empty(self):
+        self.assertEquals(len(self.bob.get_profile().current_positions()), 0)
+        for pos in Position.objects.all():
+            self.assertEquals(pos.holdings.count(), 0)
+    
+    def test_current_positions(self):
+        p = self.bob.get_profile()
+        
+        p.add_position(self.reader)
+        self.assertEquals(p.positions.count(), 1)
+        self.assertEquals(p.current_positions()[0].position, self.reader)
+        
+        p.add_position(self.reporter)
+        self.assertEquals(p.positions.count(), 2)
+        self.assertEquals(set([ph.position for ph in p.current_positions()]), 
+                          set([self.reader, self.reporter]))
+    
