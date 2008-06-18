6c69553b7a038ba8409ba8e8a255bc0a7bdf891c
Dougal Sutherland <dougal@gmail.com>
2008-06-18 23:19:23 +0000

    updated urlconf

--- gazjango/urls.py
+++ gazjango/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls.defaults import *
-import articles
+import articles, issues
 
 urlpatterns = patterns('',
-                       (r'^(\d{4})/$', 'articles.views.archive_year'),
-                       (r'^(\d{4})/(\d{1,2})/$', 'articles.views.archive_month'),
-                       (r'^(\d{4})/(\d{1,2})/(\d{1,2})/([\w-]+)/$'),
-                       (r'^(\d{4})/(\d{1,2})/(\d{1,2})/([\w-]+)/$', 'articles.views.article_page')
+    (r'^/$', 'articles.views.index'),
+    
+    (r'^(\d{4})/$',                              'articles.views.archive_year'),
+    (r'^(\d{4})/(\d{1,2})/$',                    'articles.views.archive_month'),
+    (r'^(\d{4})/(\d{1,2})/(\d{1,2})/$',          'articles.views.archive_day'),
+    (r'^(\d{4})/(\d{1,2})/(\d{1,2})/([\w-]+)/$', 'articles.views.article_page'),
+    
+    (r'^issue/$', 'issues.views.today'),
+    (r'^issue/(\d{4})/(\d{1,2})/(\d{1,2})/', 'issues.views.for_date')
 )
