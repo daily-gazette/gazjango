150968c1fb47afd23a028c6555487fc3c46ac007
Dougal Sutherland <dougal@gmail.com>
2008-07-21 00:42:43 +0000

    refactored settings to work better with the server, etc. now the way to do it is: on checkout, either link settings_dev.py to settings.py (if you want to use sqlite) or create a new settings.py, starting with "from settings_base import *" and setting up the database or whatever.

---
+++ gazjango/settings_dev.py
@@ -0,0 +1,8 @@
+from settings_base import *
+
+DATABASE_ENGINE   = 'sqlite3'
+DATABASE_NAME     = './development.db'
+DATABASE_USER     = ''
+DATABASE_PASSWORD = ''
+DATABASE_HOST     = ''
+DATABASE_PORT     = ''
