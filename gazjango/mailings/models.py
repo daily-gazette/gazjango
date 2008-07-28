d32f149528add024e7aaefb0c80c3333232cd886
Dougal Sutherland <dougal@gmail.com>
2008-07-28 01:54:59 +0000

    added start of a send-emails app. i'm not totally sure how we're going to actually send the emails; there's a project on google code (django-mailer) that looks like it's aiming to solve this problem, but i'm not sure how usable it is just yet. maybe we can help get that up to speed? until then, anyway, there's no reason (besides its suckage) not to keep sending out with phplist like we've been doing.

---
+++ gazjango/mailings/models.py
@@ -0,0 +1,79 @@
+from django.db        import models
+from django.db.models import signals
+from django.dispatch  import dispatcher
+from accounts.models import UserProfile, UserKind
+import random
+
+class IssueType(models.Model):
+    """
+    What template should be used for this subscriber's issues.
+    
+    Include plain text, regular HTML with images, optimized-for-swatmail,
+    full-article-text.
+    """
+    name = models.CharField(max_length=60)
+    slug = models.SlugField(unique=True)
+    
+    def __unicode__(self):
+        return self.slug
+    
+
+class ActiveSubscribersManager(models.Manager):
+    def get_query_set(self):
+        orig = super(ActiveSubscribersManager, self).get_query_set()
+        return orig.filter(unsubscribed=None)
+    
+
+class Subscriber(models.Model):
+    """
+    A subscriber, who gets issues in their inbox when we publish. Either 
+    linked to a UserProfile or has a name, email, and UserKind.
+    
+    When someone unsubscribes, we set `unsubscribed` to that day and leave
+    their data around, so that we can get pretty graphs of subscription
+    numbers or whatever.
+    
+    The confirmation key is quasi-secret; it serves in lieu of a password
+    for management things (unsubscribing, changing preferences) as well as
+    
+    """    
+    _name  = models.CharField(max_length=40)
+    _email = models.EmailField(unique=True)
+    _kind  = models.ForeignKey(UserKind, null=True)
+    user = models.ForeignKey(UserProfile, null=True, unique=True)
+    
+    name  = property(lambda self: self.user.name  if self.user else self._name)
+    email = property(lambda self: self.user.email if self.user else self._email)
+    kind  = property(lambda self: self.user.kind  if self.user else self._kind)
+    
+    subscribed   = models.DateField(auto_now_add=True)
+    modified     = models.DateField(auto_now=True)
+    unsubscribed = models.DateField(null=True)
+    
+    issue_type   = models.ForeignKey(IssueType)
+    racy_content = models.BooleanField(default=True)
+    
+    is_confirmed = models.BooleanField(default=False)
+    confirmation_key = models.CharField(max_length=15)
+    
+    objects = models.Manager()
+    active = ActiveSubscribersManager()
+    
+    def randomize_confirmation_key(self):
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        key = ''.join(random.choice(chars) for x in xrange(30))
+        self.confirmation_key = key
+    
+    def __unicode__(self):
+        if self.user:
+            return "%s (linked to %s)" % (self.email, self.user.username)
+        else:
+            return self.email
+    
+
+def set_default_key(sender, instance):
+    if not instance.confirmation_key:
+        instance.randomize_confirmation_key()
+        instance.save()
+
+dispatcher.connect(set_default_key, signal=signals.post_init, sender=Subscriber)
