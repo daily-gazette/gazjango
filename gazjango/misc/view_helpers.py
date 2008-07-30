cf31d8c446e4239ada5151d9c2142272a359923c
Dougal Sutherland <dougal@gmail.com>
2008-07-30 18:43:53 +0000

    changed get_by_date_or_404  and filter_by_date to cast year/month/day into integers; it was causing problems

--- gazjango/misc/view_helpers.py
+++ gazjango/misc/view_helpers.py
@@ -2,16 +2,18 @@ from django.shortcuts import get_object_or_404
 
 def get_by_date_or_404(model, year, month, day, field='pub_date', **oth):
     d = oth
-    d.update({field+'__year': year, field+'__month': month, field+'__day': day})
+    d[field + '__year']  = int(year)
+    d[field + '__month'] = int(month)
+    d[field + '__day']   = int(day)
     return get_object_or_404(model, **d)
 
 def filter_by_date(qset, year=None, month=None, day=None, field='pub_date'):
     args = {}
     if year:
-        args[field + '__year'] = year
+        args[field + '__year'] = int(year)
         if month:
-            args[field + '__month'] = month
+            args[field + '__month'] = int(month)
             if day:
-                args[field + '__day'] = day
+                args[field + '__day'] = int(day)
     return qset.filter(**args)
 
