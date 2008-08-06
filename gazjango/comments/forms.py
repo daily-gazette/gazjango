f2aaef6a310c56036e6a6b2d3c1f2392addf2ea6
Dougal Sutherland <dougal@gmail.com>
2008-08-06 04:10:23 +0000

    changed to a better way of handling the comment form: only one Form, and a helper to customize it based on whether you're logged in

--- gazjango/comments/forms.py
+++ gazjango/comments/forms.py
@@ -1,14 +1,31 @@
 from django import forms
 
-class BaseCommentForm(forms.Form):
+def make_comment_form(data=None, logged_in=False, **kwargs):
+    if data:
+        form = CommentForm(data, **kwargs)
+    else:
+        form = CommentForm(**kwargs)
+    if logged_in:
+        form.fields['name'].widget = forms.TextInput(
+            attrs={ 'id': 'filled-name', 'disabled': True }
+        )
+        
+        form.fields['email'].widget = forms.HiddenInput()
+        form.fields['email'].required = False
+        
+        form.fields['anonymous'].widget = forms.CheckboxInput(
+            attrs={ 'onclick': 'toggleNameDisabled();' }
+        )
+    else:
+        form.fields['anonymous'].widget = forms.CheckboxInput(
+            attrs={ 'display': 'none' }
+        )
+        form.fields['anonymous'].initial = True
+    return form
+
+class CommentForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea(attrs={'rows': 8, 'cols': 52}))
-
-class AnonCommentForm(BaseCommentForm):
-    name  = forms.CharField(max_length=100)
+    
+    anonymous = forms.BooleanField(widget=forms.HiddenInput())
+    name = forms.CharField(max_length=100)
     email = forms.EmailField()
-
-class UserCommentForm(BaseCommentForm):
-    anonymous = forms.BooleanField(widget=forms.CheckboxInput(
-                    attrs={'onclick': 'toggleNameDisabled()'}))
-    name = forms.CharField(widget=forms.TextInput(
-                    attrs={'id': 'filled-name', 'disabled': True}))
