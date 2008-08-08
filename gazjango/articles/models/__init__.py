0c7726ba3ec12e6f8af6024a6e1391749c5f3a75
Dougal Sutherland <dougal@gmail.com>
2008-08-08 02:42:41 +0000

    oops...forgot to import SectionSpecial :)

--- gazjango/articles/models/__init__.py
+++ gazjango/articles/models/__init__.py
@@ -1,6 +1,7 @@
 from stories       import Article, ArticleRevision, Format
 from sections      import Section, Subsection
-from specials      import Special, SpecialsCategory, DummySpecialTarget
+from specials      import Special, SpecialsCategory
+from specials      import DummySpecialTarget, SectionSpecial
 from concepts      import StoryConcept
 from photo_spreads import PhotoSpread, PhotoInSpread
 import tagging
