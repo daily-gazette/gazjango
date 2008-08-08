b57e21c6f6cee535389c436990d81ddd59665bba
Dougal Sutherland <dougal@gmail.com>
2008-08-08 21:52:19 +0000

    fixed a stupid thing i did

--- gazjango/urls.py
+++ gazjango/urls.py
@@ -44,7 +44,7 @@ urlpatterns += patterns('articles.views',
     (r'^%(year)s/%(month)s/%(day)s/%(slug)s/print/$'   % reps, 'article', {'print_view': True}, 'print'),
     (r'^%(year)s/%(month)s/%(day)s/%(slug)s/email/$'   % reps, 'email_article', {}, 'email'),
     
-    (r'^%(year)s/%(month)s/%(day)s/%(slug)s/show-comment/%(num)s/$' % reps, {}, 'get_comment_text'),
+    (r'^%(year)s/%(month)s/%(day)s/%(slug)s/show-comment/%(num)s/$' % reps, 'get_comment_text'),
     
     (r'^archives/$', 'articles', {}, 'archives'),
     (r'^(?:archives/)?%(year)s/$'                   % reps, 'articles'),
