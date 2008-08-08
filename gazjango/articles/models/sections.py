fedcb704a256b77368d494006351cccbd02d9601
Dougal Sutherland <dougal@gmail.com>
2008-08-08 02:24:10 +0000

    added a this-subsection-is-done flag, for when columns have ended

--- gazjango/articles/models/sections.py
+++ gazjango/articles/models/sections.py
@@ -36,12 +36,18 @@ class Section(models.Model):
 class Subsection(models.Model):
     """
     A subsection: Ask the Gazette, College News, a specific column, whatever.
+    
+    If `is_over` is set, don't have it show up in the list in the admin. Used
+    mainly for columns that are no longer running. There's nothing app-level
+    that actually prevents new articles using it, however.
     """
     
     name = models.CharField(max_length=40)
     slug = models.SlugField(unique=True)
     description = models.CharField(max_length=250, blank=True)
     section = models.ForeignKey(Section, related_name="subsections")
+    
+    is_over = models.BooleanField(default=False)
     
     class Meta:
         app_label = 'articles'
