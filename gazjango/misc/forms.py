ed43ef6560714554baf363144a11fdebb8b319ac
Dougal Sutherland <dougal@gmail.com>
2008-08-14 03:51:29 +0000

    made MultipleForeignKeyField work

--- gazjango/misc/forms.py
+++ gazjango/misc/forms.py
@@ -100,24 +100,21 @@ class AuthorInputField(forms.MultipleChoiceField):
         return ps
     
 
-def _find_choices_list(model, show_field):
-    return lambda: [(fields[0], reduce(lambda a,b: a or b, fields[1:]))
-                    for fields in model.objects.values_list('pk', *show_field)]
+def _first(lst):
+    reduce(lambda a, b: a or b, lst)
 
-def _find_choices_single(model, show_fields):
-    return lambda: [(pk, oth) for pk, oth 
-                    in model.objects.values_list('pk', show_field)]
-
-
-class ForeignKeyField(forms.ChoiceField):    
+class ForeignKeyField(forms.ChoiceField):
+    def _find_choices(self):
+        return [(fields[0], _first(fields[1:])) for fields
+                in self.model.objects.values_list('pk', *self.show_field)]
+    
     def __init__(self, model, show_field, *args, **kwargs):
         self.model = model
-        self.show_field = show_field
         if isinstance(show_field, (tuple, list)):
-            self._find_choices = _find_choices_list(model, show_field)
+            self.show_field = show_field
         else:
-            self._find_choices = _find_choices_single(model, show_field)
-            
+            self.show_field = (show_field,)
+        
         kwargs['choices'] = self._find_choices()
         super(ForeignKeyField, self).__init__(*args, **kwargs)
     
@@ -133,19 +130,62 @@ class ForeignKeyField(forms.ChoiceField):
 
 # multiple inheritance is confusing, so we're going to avoid it
 class MultipleForeignKeyField(forms.MultipleChoiceField):
-    def __init__(self, model, show_field, *args, **kwargs):
+    def __init__(self, model, show_field, group=None, group_show_field=None,
+                 *args, **kwargs):
         self.model = model
-        self.show_field = show_field
         if isinstance(show_field, (tuple, list)):
-            self._find_choices = _find_choices_list(model, show_field)
+            self.show_fields = show_field
         else:
-            self._find_choices = _find_choices_single(model, show_field)
+            self.show_fields = (show_field,)
+        self.group = group
+        self.group_show_field = group_show_field
         
         kwargs['choices'] = self._find_choices()
         super(MultipleForeignKeyField, self).__init__(*args, **kwargs)
     
+    def _find_choices(self):
+        if self.group:
+            # how to label groups
+            if self.group_show_field:
+                label = lambda x: \
+                         x.__getattribute__(self.group_show_field) if x else x
+            else:
+                label = lambda x: unicode(x) if x else x
+            
+            # how to show each model
+            def show(instance):
+                for field in self.show_fields:
+                    if instance.__dict__[field]:
+                        return instance.__dict__[field]
+                return unicode(instance)
+            
+            # organize into {label: [(pk, name), (pk, name), ...]}
+            results = {}
+            for instance in self.model.objects.all():
+                grp = label(instance.__getattribute__(self.group))
+                results.setdefault(grp, []).append((instance.pk, show(instance)))
+            
+            # dict => list
+            choices = results.items()
+            
+            # sort the groups
+            choices.sort(key=lambda lst: lst[0])
+            if len(choices) and choices[0][0] is None:
+                choices.append( ('Other', choices.pop(0)[1]) )
+            
+            # sort within each group
+            second_lower = lambda pair: pair[1].lower()
+            for label, items in choices:
+                items.sort(key=second_lower)
+            
+            return choices
+        
+        else:
+            return [(fields[0], _first(fields[1:])) for fields
+                    in self.model.objects.values_list('pk', *self.show_fields)]
+    
     def refresh_choices(self):
-        self.choices = self._find_choices
+        self.choices = self._find_choices()
     
     def clean(self, value):
         values = []
