bbfeaead31797f5f3bbdb7ebcb9ac551cf58b20d
Dougal Sutherland <dougal@gmail.com>
2008-08-15 22:29:13 +0000

    add the humanize library, which means we have another exception in init_dev_data.sh

--- gazjango/init_dev_data.sh
+++ gazjango/init_dev_data.sh
@@ -10,7 +10,7 @@ read resp
 
 echo -n "clearing database..." && \
 $python manage.py sqlclear \
-$($python -c 'import settings; print " ".join(s.split(".")[-1] for s in settings.INSTALLED_APPS if s not in ("gazjango.misc", "django.contrib.admindocs"))') \
+$($python -c 'import settings; print " ".join(s.split(".")[-1] for s in settings.INSTALLED_APPS if s not in ("gazjango.misc", "django.contrib.admindocs", "django.contrib.humanize"))') \
 | $python manage.py dbshell && \
 echo "done." && \
 echo -n "syncing database..." && \
