71279c38bbd4870d1cd644a17d99f38a9e59fe81
Dougal Sutherland <dougal@gmail.com>
2008-09-22 05:02:07 +0000

    add an admin user

--- gazjango/init_dev_data.py
+++ gazjango/init_dev_data.py
@@ -128,6 +128,11 @@ neena = make_user('neena', 'Neena', 'Cherayil', groups=[reader_group, reporter_g
 lauren = make_user('lauren', 'Lauren', 'Stokes', groups=[reader_group, reporter_group])
 brandon = make_user('brandon', 'Brandon', 'Lee Wolff', groups=[reader_group])
 
+admin = make_user('admin', "The", "Daily Gazette", 'dailygazette@swarthmore.edu', groups=[reader_group, admin_group])
+admin.set_password('test')
+admin.is_superuser = True
+admin.save()
+
 dougal = make_user('dougal', 'Dougal', 'Sutherland', 'dsuther1@swarthmore.edu', groups=[reader_group, admin_group])
 dougal.password = 'sha1$8b8f9$e9526bcd787b71bc9fb1f68965815e2508333a6e'
 dougal.is_superuser = True
