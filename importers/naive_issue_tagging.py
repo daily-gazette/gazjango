2c3c7bc63ad3689f1e2de4e6221c2b06f1f0e18d
Dougal Sutherland <dougal@gmail.com>
2008-10-17 06:20:02 +0000

    basic issue tagger

---
+++ importers/naive_issue_tagging.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import os.path
+import sys
+
+sys.path.extend([os.path.abspath(x) for x in (
+    '..',
+    '../gazjango',
+    os.path.join(__file__, '..'),
+    os.path.join(__file__, '../gazjango'),
+)])
+
+import settings
+import django.core.management
+django.core.management.setup_environ(settings)
+
+import datetime
+from gazjango.articles.models import Article
+from gazjango.issues.models   import Issue, Weather, WeatherJoke, Menu
+
+default_joke = WeatherJoke.objects.create(
+    date=datetime.date(1864, 9, 1),
+    line_one="This issue is from the time when we didn't save our weather jokes.",
+    line_two="Utterly barbaric, I know.",
+    line_three="But honestly? You're not missing much.",
+)
+
+default_menu = Menu.objects.create(
+    date=datetime.date(1864, 9, 1),
+    closed=True,
+    message="Sharples was effectively closed this day, because we don't remember what they served.",
+)
+
+default_weather = Weather.objects.create(
+    date=datetime.date(1864, 9, 1),
+    today='',
+    tomorrow='',
+    tonight=''
+)
+
+zero = datetime.timedelta()
+one_day = datetime.timedelta(days=1)
+
+print 'adding articles to issues...',
+
+for article in Article.objects.all():
+    date = article.pub_date.date() + (one_day if article.pub_date.hour > 5 else zero)
+    issue, created = Issue.objects.get_or_create(date=date,
+        defaults={ 'menu': default_menu, 'weather': default_weather, 'joke': default_joke }
+    )
+    issue.add_article(article)
+
+print 'done.'
