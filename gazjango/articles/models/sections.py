298a6e3404e04add72eabc9c685610f6e5e4ec71
Dougal Sutherland <dougal@gmail.com>
2008-10-19 19:30:54 +0000

    allow any length for subsection descriptions

--- gazjango/articles/models/sections.py
+++ gazjango/articles/models/sections.py
@@ -58,7 +58,7 @@ class Subsection(models.Model):
     
     name = models.CharField(max_length=40)
     slug = models.SlugField(unique=True)
-    description = models.CharField(max_length=250, blank=True)
+    description = models.TextField(blank=True)
     section = models.ForeignKey(Section, related_name="subsections")
     
     class Meta:
