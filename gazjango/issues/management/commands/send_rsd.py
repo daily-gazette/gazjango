16b3daad4dd676ca9a900744e49ac43b906d5e83
Dougal Sutherland <dougal@gmail.com>
2008-10-20 08:36:53 +0000

    set for_email in send_rsd

--- gazjango/issues/management/commands/send_rsd.py
+++ gazjango/issues/management/commands/send_rsd.py
@@ -10,6 +10,7 @@ RSD_TEXT_LIST = 'reserved-text-students@sccs.swarthmore.edu'
 class Command(NoArgsCommand):
     def handle_noargs(self, **options):
         dummy_request = HttpRequest()
+        dummy_request.GET['for_email'] = True
         html_content = rsd_now(dummy_request).content
         text_content = rsd_now(dummy_request, plain=True).content
         
