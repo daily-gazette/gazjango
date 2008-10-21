4c048dbff2d2aa506bbc68a98783ab64941130be
Dougal Sutherland <dougal@gmail.com>
2008-10-21 00:39:30 +0000

    improve announcement admin

--- gazjango/announcements/admin.py
+++ gazjango/announcements/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from gazjango.announcements.models import Announcement
 
 class AnnouncementAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('__unicode__', 'is_published', 'is_event', 'date_start', 'date_end', 'kind')
+    list_filter = ('is_published', 'is_event', 'kind')
 admin.site.register(Announcement, AnnouncementAdmin)
