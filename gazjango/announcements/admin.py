41ed05139e2a649fe8a89abb43441b742507780b
Dougal Sutherland <dougal@gmail.com>
2008-10-21 00:41:49 +0000

    remove filtering by is_event, since it's not a real field

--- gazjango/announcements/admin.py
+++ gazjango/announcements/admin.py
@@ -3,5 +3,5 @@ from gazjango.announcements.models import Announcement
 
 class AnnouncementAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'is_published', 'is_event', 'date_start', 'date_end', 'kind')
-    list_filter = ('is_published', 'is_event', 'kind')
+    list_filter = ('is_published', 'kind')
 admin.site.register(Announcement, AnnouncementAdmin)
