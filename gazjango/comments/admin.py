58fa0fbfb7eb0448c720503aefc705b89fc961bc
Dougal Sutherland <dougal@gmail.com>
2008-10-21 01:05:16 +0000

    comment admin tweaks

--- gazjango/comments/admin.py
+++ gazjango/comments/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from gazjango.comments.models import PublicComment
 
 class PublicCommentAdmin(admin.ModelAdmin):
-    list_display = ('article_name', 'display_name', 'is_anonymous', 'is_spam', 'is_approved')
+    list_display = ('article_name', 'number', 'display_name', 'is_anonymous', 'is_spam', 'is_approved')
+    list_display_links = ('number',)
     list_filter = ('is_spam', 'is_approved')
 admin.site.register(PublicComment, PublicCommentAdmin)
