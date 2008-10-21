b135a656c251b2d64c024982c7c895415820c879
Dougal Sutherland <dougal@gmail.com>
2008-10-21 00:59:41 +0000

    did it again

--- gazjango/comments/admin.py
+++ gazjango/comments/admin.py
@@ -3,5 +3,5 @@ from gazjango.comments.models import PublicComment
 
 class PublicCommentAdmin(admin.ModelAdmin):
     list_display = ('subject', 'display_name', 'is_anonymous', 'is_spam', 'is_approved')
-    list_filter = ('is_anonymous', 'is_spam', 'is_approved')
+    list_filter = ('is_spam', 'is_approved')
 admin.site.register(PublicComment, PublicCommentAdmin)
