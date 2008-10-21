a13b21b811ca1d4fed362b436c5484a47fe49c84
Dougal Sutherland <dougal@gmail.com>
2008-10-21 03:26:14 +0000

    improve jobs admin

--- gazjango/jobs/admin.py
+++ gazjango/jobs/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from gazjango.jobs.models import JobListing
 
 class JobAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'is_published', 'pub_date', 'is_filled')
+    list_filter = ('is_published', 'is_filled')
+    date_hierarchy = 'pub_date'
 admin.site.register(JobListing, JobAdmin)
