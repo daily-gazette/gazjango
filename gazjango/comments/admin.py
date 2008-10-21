03b4b6b0e7a6800e1ded2b15550e08ca7e4e40a5
Dougal Sutherland <dougal@gmail.com>
2008-10-21 01:00:29 +0000

    try this?

--- gazjango/comments/admin.py
+++ gazjango/comments/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from gazjango.comments.models import PublicComment
 
 class PublicCommentAdmin(admin.ModelAdmin):
-    list_display = ('subject', 'display_name', 'is_anonymous', 'is_spam', 'is_approved')
+    list_display = ('article__headline', 'display_name', 'is_anonymous', 'is_spam', 'is_approved')
     list_filter = ('is_spam', 'is_approved')
 admin.site.register(PublicComment, PublicCommentAdmin)
