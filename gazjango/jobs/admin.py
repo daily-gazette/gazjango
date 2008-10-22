a12d351ed1d78f743b3ed3a284e6cbfadca5b884
Dougal Sutherland <dougal@gmail.com>
2008-10-22 01:39:02 +0000

    add contact_name to the jobs admin

--- gazjango/jobs/admin.py
+++ gazjango/jobs/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from gazjango.jobs.models import JobListing
 
 class JobAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_published', 'pub_date', 'is_filled')
+    list_display = ('name', 'is_published', 'pub_date', 'contact_name', 'is_filled')
     list_filter = ('is_published', 'is_filled')
     date_hierarchy = 'pub_date'
 admin.site.register(JobListing, JobAdmin)
