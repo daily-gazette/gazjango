436e614973dde11ea706806d910e807fdd5cf3aa
Miles Skorpen <miles@sccs.swarthmore.edu>
2008-10-26 17:19:54 +0000

    adding more to the staff model

--- gazjango/articles/models/concepts.py
+++ gazjango/articles/models/concepts.py
@@ -36,8 +36,7 @@ class StoryConcept(models.Model):
     status = models.CharField(max_length=1, choices=STATUSES, default='a')
     
     users   = models.ManyToManyField(UserProfile, related_name="assignments")
-    editors   = models.ManyToManyField(UserProfile, related_name="overseer")
-
+    
     article = models.ForeignKey(Article, null=True, unique=True,
                                 related_name="concepts")
     
