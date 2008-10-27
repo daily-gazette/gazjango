71527d6d7e4a43c229fc64accc328506b883ab12
Miles Skorpen <miles@sccs.swarthmore.edu>
2008-10-27 22:55:53 +0000

    updating the staff model

--- gazjango/articles/views.py
+++ gazjango/articles/views.py
@@ -251,11 +251,9 @@ def homepage(request, template="index.html"):
     return render_to_response(template, data, context_instance=rc)
     
 def staff(request,  template="staff/index.html"):
-	user = get_user_profile(request),
+	user = get_user_profile(request)
 	
-	personal = StoryConcept.unpublished.get_concepts(users='user.username_for_name()')
-	unclaimed = StoryConcept.unpublished.get_concepts(users='')
-	claimed = StoryConcept.unpublished.get_concepts(users!='' or users!='user.username_for_name()')
+	personal, unclaimed, claimed = StoryConcept.unpublished.get_concepts(user=user)
 	data = {
 		'personal': personal,
 		'unclaimed': unclaimed,
