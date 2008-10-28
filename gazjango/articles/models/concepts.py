7229d311e012f642270ad1428e4b36d17250ac20
Dougal Sutherland <dougal@gmail.com>
2008-10-28 04:58:53 +0000

    un-list get_concepts

--- gazjango/articles/models/concepts.py
+++ gazjango/articles/models/concepts.py
@@ -18,9 +18,9 @@ class UnpublishedConceptsManager(models.Manager):
         
         base = base or self
         
-        users = list(base.filter(users=user).order_by('due'))
-        others = list(base.exclude(users=user).exclude(users=None).order_by('due'))
-        unclaimed = list(base.filter(users=None).order_by('due'))
+        users = base.filter(users=user).order_by('due')
+        others = base.exclude(users=user).exclude(users=None).order_by('due')
+        unclaimed = base.filter(users=None).order_by('due')
         
         return [users, others, unclaimed]
     
