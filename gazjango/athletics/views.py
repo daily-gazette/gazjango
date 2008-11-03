a6bcccbac63c28579dc95f556a14e663d920aa7e
Miles Skorpen <miles@sccs.swarthmore.edu>
2008-11-03 05:39:21 +0000

    adding a new athletics site

---
+++ gazjango/athletics/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts       import get_object_or_404, render_to_response
+from django.template        import RequestContext
+
+from gazjango.articles.models  import Section
+from gazjango.athletics.models import Team, Game
+
+from gazjango.articles.models      import Article   
+    
+
+def athletics(request):
+    'View for "athletic" team pages.'     
+        
+    start_trimester = Game.objects.latest().team.trimester
+    current_trimester = Team.objects.filter(trimester=start_trimester).order_by("sport","gender") 
+    
+    section = Section.objects.get(slug='athletics')
+    article_list = section.published_articles().order_by("-pub_date")
+    
+    
+    rc = RequestContext(request, { 'trimester': current_trimester, 'article_list': article_list })
+    return render_to_response('athletics/index.html', context_instance=rc)
+
+
+
+def team(request, slug):
+    'View for "athletic" team pages.'
+    
+    team = get_object_or_404(Team, slug=slug)
+        
+    games_list = Team.games_for_semester(False)
+    
+    
+    rc = RequestContext(request, { 'team': team })
+    return render_to_response('athletics/team.html', context_instance=rc)
