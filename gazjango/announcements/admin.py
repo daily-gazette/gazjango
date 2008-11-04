c7183e6e96bb49e1ed67c6001b01b07dcd31d413
Dougal Sutherland <dougal@gmail.com>
2008-11-04 22:00:48 +0000

    tweak announcement admin

--- gazjango/announcements/admin.py
+++ gazjango/announcements/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from gazjango.announcements.models import Announcement
 
 class AnnouncementAdmin(admin.ModelAdmin):
-    list_display = ('__unicode__', 'is_published', 'is_event', 'date_start', 'date_end', 'kind')
+    list_display = ('slug', 'name', 'brief_excerpt', 'is_published', 'is_event', 'date_start', 'date_end')
     list_filter = ('is_published', 'kind')
 admin.site.register(Announcement, AnnouncementAdmin)
