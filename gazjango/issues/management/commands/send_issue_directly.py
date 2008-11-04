52c917f885897864f765dc97253a4bb30089356d
Dougal Sutherland <dougal@gmail.com>
2008-11-04 07:48:57 +0000

    robustify send_issue_directly

--- gazjango/issues/management/commands/send_issue_directly.py
+++ gazjango/issues/management/commands/send_issue_directly.py
@@ -4,6 +4,7 @@ from django.http                 import HttpRequest
 from gazjango.subscriptions.models import Subscriber
 from gazjango.issues.views         import latest_issue
 import datetime
+import traceback
 import sys
 import smtplib
 
@@ -27,20 +28,34 @@ class Command(NoArgsCommand):
         print 'starting to send emails, ' + datetime.datetime.now().strftime("%c")
         
         connection = SMTPConnection()
-        for subscriber in Subscriber.issues.all():
-            if subscriber.plain_text:
-                content = text_content if subscriber.racy_content else tame_text_content
-                msg = EmailMessage(subject, content, from_email, [subscriber.email])
+        to_send = [(subscriber, 0) for subscriber in Subscriber.issues.all()]
+        
+        while to_send:
+            subscriber, count = to_send.pop(0)
+            if count > 5:
+                print 'giving up on %s' % subscriber
             else:
-                text = text_content if subscriber.racy_content else tame_text_content
-                html = html_content if subscriber.racy_content else tame_html_content
-                msg = EmailMultiAlternatives(subject, text, from_email, [subscriber.email])
-                msg.attach_alternative(html, 'text/html')
-            
-            try:
-                connection.send_messages([msg])
-            except smtplib.SMTPRecipientsRefused:
-                sys.stderr.write("recipient refused: %s\n" % subscriber.email)
+                if subscriber.plain_text:
+                    content = text_content if subscriber.racy_content else tame_text_content
+                    msg = EmailMessage(subject, content, from_email, [subscriber.email])
+                else:
+                    text = text_content if subscriber.racy_content else tame_text_content
+                    html = html_content if subscriber.racy_content else tame_html_content
+                    msg = EmailMultiAlternatives(subject, text, from_email, [subscriber.email])
+                    msg.attach_alternative(html, 'text/html')
+
+                try:
+                    connection.send_messages([msg])
+                except smtplib.SMTPRecipientsRefused:
+                    sys.stderr.write("recipient refused: %s\n" % subscriber.email)
+                except Exception, e:
+                    sys.stderr.write("%s: error sending to %s\n%s: %s\n%s\n\n" % 
+                            (datetime.datetime.now().strftime("%c"),
+                             subscriber,
+                             e.__class__.__name__,
+                             e.message,
+                             traceback.format_exc()))
+                    to_send.append((subscriber, count + 1))
         
         print 'done sending emails, ' + datetime.datetime.now().strftime("%c")
     
