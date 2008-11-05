99019ca7cf107d353b40b5565200f2b4bc0b2719
Dougal Sutherland <dougal@gmail.com>
2008-11-05 02:20:23 +0000

    add an error check (sort of) to send_issue_directly

--- gazjango/issues/management/commands/send_issue_directly.py
+++ gazjango/issues/management/commands/send_issue_directly.py
@@ -1,5 +1,5 @@
 from django.core.management.base import NoArgsCommand
-from django.core.mail            import EmailMessage, EmailMultiAlternatives, SMTPConnection
+from django.core                 import mail
 from django.http                 import HttpRequest
 from gazjango.subscriptions.models import Subscriber
 from gazjango.issues.views         import latest_issue
@@ -19,6 +19,10 @@ class Command(NoArgsCommand):
         tame_html_content = latest_issue(dummy_request).content
         tame_text_content = latest_issue(dummy_request, plain=True).content
         
+        if not (200 <= html_content.status_code < 300):
+            mail.mail_admins('OH NO: %s IN SENDING THE ISSUE' % html_content.status_code,
+                             'Check /issue and ~/issue_output. Sent it anyway.')
+        
         # Thursday, October 2, 2008; not Thursday, October 02, 2008
         today = datetime.date.today()
         subject = today.strftime("%A, %B !!, %Y").replace("!!", str(today.day))
@@ -27,7 +31,7 @@ class Command(NoArgsCommand):
         
         print 'starting to send emails, ' + datetime.datetime.now().strftime("%c")
         
-        connection = SMTPConnection()
+        connection = mail.SMTPConnection()
         to_send = [(subscriber, 0) for subscriber in Subscriber.issues.all()]
         
         while to_send:
@@ -38,11 +42,11 @@ class Command(NoArgsCommand):
                 
             if subscriber.plain_text:
                 content = text_content if subscriber.racy_content else tame_text_content
-                msg = EmailMessage(subject, content, from_email, [subscriber.email])
+                msg = mail.EmailMessage(subject, content, from_email, [subscriber.email])
             else:
                 text = text_content if subscriber.racy_content else tame_text_content
                 html = html_content if subscriber.racy_content else tame_html_content
-                msg = EmailMultiAlternatives(subject, text, from_email, [subscriber.email])
+                msg = mail.EmailMultiAlternatives(subject, text, from_email, [subscriber.email])
                 msg.attach_alternative(html, 'text/html')
             
             try:
