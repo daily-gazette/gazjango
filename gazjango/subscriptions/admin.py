2fb57ed171cade608203e8195a7dbdc8d6d4a648
Dougal Sutherland <dougal@gmail.com>
2008-11-05 16:30:36 +0000

    tweak subscriber admin

--- gazjango/subscriptions/admin.py
+++ gazjango/subscriptions/admin.py
@@ -3,6 +3,8 @@ from gazjango.subscriptions.models import Subscriber
 
 class SubscriberAdmin(admin.ModelAdmin):
     exclude = ('confirmation_key',)
+    list_display = ('email', 'name', 'receive')
+    list_filter = ('receive',)
 admin.site.register(Subscriber, SubscriberAdmin)
 
 
