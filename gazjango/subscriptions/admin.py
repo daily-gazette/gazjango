0255aba472823d013160d6ac861e6a43681cb5f5
Dougal Sutherland <dougal@gmail.com>
2008-11-07 15:53:21 +0000

    subscription admin tweaks

--- gazjango/subscriptions/admin.py
+++ gazjango/subscriptions/admin.py
@@ -5,7 +5,7 @@ class SubscriberAdmin(admin.ModelAdmin):
     exclude = ('confirmation_key',)
     list_display = ('email', 'name', 'kind', 'receive', 'is_active', 'racy_content')
     list_filter = ('receive', 'racy_content')
-    search_fields = ('email', 'name')
+    search_fields = ('_email', '_name', 'user__email', 'user__name')
 admin.site.register(Subscriber, SubscriberAdmin)
 
 
