8e5123fb64337196fb4b0838ad8e520559406d41
Dougal Sutherland <dougal@gmail.com>
2008-11-17 05:22:10 +0000

    admin tweaks

--- gazjango/issues/admin.py
+++ gazjango/issues/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from gazjango.issues.models import Issue, WeatherJoke
 
 class IssueAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = 'date'
+    filter_horizontal = ('articles')
 admin.site.register(Issue, IssueAdmin)
 
 class WeatherJokeAdmin(admin.ModelAdmin):
-    pass
+    date_hierarchy = 'date'
+    list_display = ('date', 'line_one')
 admin.site.register(WeatherJoke, WeatherJokeAdmin)
