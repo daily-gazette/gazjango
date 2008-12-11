b927e6464c868c91966d568e47b4f0d172068f27
Dougal Sutherland <dougal@gmail.com>
2008-12-11 20:06:19 +0000

    allow searching for comments

--- gazjango/comments/admin.py
+++ gazjango/comments/admin.py
@@ -5,4 +5,6 @@ class PublicCommentAdmin(admin.ModelAdmin):
     list_display = ('article_name', 'number', 'display_name', 'is_anonymous', 'is_spam', 'is_approved')
     list_display_links = ('number',)
     list_filter = ('is_spam', 'is_approved')
+    search_fields = ('text', 'article__name', 'name', 'email', 
+                     'user__user__first_name', 'user__user__last_name', 'user__user__email')
 admin.site.register(PublicComment, PublicCommentAdmin)
