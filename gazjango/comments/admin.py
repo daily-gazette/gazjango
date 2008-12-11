575587d6116854afd3b15fc54e051c667b4772f7
Dougal Sutherland <dougal@gmail.com>
2008-12-11 20:07:22 +0000

    oops, wrong field name

--- gazjango/comments/admin.py
+++ gazjango/comments/admin.py
@@ -5,6 +5,6 @@ class PublicCommentAdmin(admin.ModelAdmin):
     list_display = ('article_name', 'number', 'display_name', 'is_anonymous', 'is_spam', 'is_approved')
     list_display_links = ('number',)
     list_filter = ('is_spam', 'is_approved')
-    search_fields = ('text', 'article__name', 'name', 'email', 
+    search_fields = ('text', 'article__headline', 'name', 'email', 
                      'user__user__first_name', 'user__user__last_name', 'user__user__email')
 admin.site.register(PublicComment, PublicCommentAdmin)
