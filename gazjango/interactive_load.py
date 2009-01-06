4ac29fd437fd33753e227e998e5c92cddb036857
Dougal Sutherland <dougal@gmail.com>
2009-01-06 23:31:33 +0000

    add a somewhat-silly interactive_load: so you can just do "from interactive_load import *" and get a reasonable set of imports

---
+++ gazjango/interactive_load.py
@@ -0,0 +1,16 @@
+# ugly, but it's hard to figure out with __import__
+
+from django import forms
+from django.core.mail import *
+from django.core.urlresolvers import reverse
+from django.http import *
+
+import settings
+for app in settings.INSTALLED_APPS:
+    exec('import %s' % app)
+    try:
+        exec('from %s.models import *' % app)
+    except ImportError:
+        pass
+from gazjango.misc.helpers import *
+from gazjango.misc.view_helpers import *
