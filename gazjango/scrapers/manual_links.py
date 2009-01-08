e57c40e53c4ddc1058a88fd9df33d48b4f328460
The Daily Gazette <dailygazette@swarthmore.edu>
2009-01-08 03:42:36 +0000

    add a link

--- gazjango/scrapers/manual_links.py
+++ gazjango/scrapers/manual_links.py
@@ -1,6 +1,7 @@
 # this isn't really a "scraper", but whatever
 
 manual_links = [
+	'<a href="http://www.treatment-centers.net">Drug Treatment</a>',
 	'<a href="http://www.istudentloan.com/">Student Loan</a>',
 	'<a href="http://www.elearners.com/colleges/">Accredited Online Schools</a>',
     '<a href="http://www.studentcreditcards.com/">student credit cards</a><br> www.studentcreditcards.com/ - student credit cards.com features informative weekly articles on credit as well as a studentcredit card comparison.',
