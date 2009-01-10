c0044a5999fb2ebfef91bdfaf5358bac1845ee85
Dougal Sutherland <dougal@gmail.com>
2009-01-10 22:53:56 +0000

    make gmaps api key a proper setting

--- gazjango/reviews/views.py
+++ gazjango/reviews/views.py
@@ -3,11 +3,15 @@ from django.template        import RequestContext
 
 from gazjango.articles.models  import Section
 from gazjango.reviews.models   import Establishment, Review
+import settings
 
 def reviews(request):
     'View for "establishment review" page.'
     
     establishments = Establishment.objects.order_by("establishment_type","name")    
     
-    rc = RequestContext(request, { 'establishments': establishments })
+    rc = RequestContext(request, { 
+        'establishments': establishments,
+        'GMAPS_API_KEY': settings.GMAPS_API_KEY,
+    })
     return render_to_response('reviews/index.html', context_instance=rc)
