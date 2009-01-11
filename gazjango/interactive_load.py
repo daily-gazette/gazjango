15702f3668b25aa2024c965114f3105e3f1cedd0
Dougal Sutherland <dougal@gmail.com>
2009-01-11 01:50:44 +0000

    interactive_load: okay, now i really need to stop being stupid. it actually works now.

--- gazjango/interactive_load.py
+++ gazjango/interactive_load.py
@@ -5,20 +5,18 @@ from django.core.mail import *
 from django.core.urlresolvers import reverse
 from django.http import *
 
-def _try_import(*app):
-    try:
-        exec('from %s import *' % '.'.join(app))
-    except ImportError:
-        pass
-
 import settings
 for app in settings.INSTALLED_APPS:
     if app.startswith('gazjango.'):
         exec('from gazjango import %s' % app[len('gazjango.'):])
     else:
         exec('import %s' % app)
-    _try_import(app, 'models')
-    _try_import(app, 'forms')
+    
+    try: exec('from %s.models import *' % app)
+    except ImportError: pass
+    
+    try: exec('from %s.forms import *' % app)
+    except ImportError: pass
 
 from gazjango.misc.helpers import *
 from gazjango.misc.view_helpers import *
