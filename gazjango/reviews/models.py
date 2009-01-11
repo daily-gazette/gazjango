b6f93557217889d6b7703e5ec73292c9957a4958
Dougal Sutherland <dougal@gmail.com>
2009-01-11 03:16:42 +0000

    use Review.get_absolute_url

--- gazjango/reviews/models.py
+++ gazjango/reviews/models.py
@@ -122,7 +122,6 @@ models.signals.pre_save.connect(geocode, sender=Establishment)
 class Review(models.Model):
     "Represents the review of an establishment."
     slug = models.SlugField()
-    
     establishment = models.ForeignKey(Establishment,related_name="reviews")
     
     COST_CHOICES = (
@@ -150,3 +149,5 @@ class Review(models.Model):
     def __unicode__(self):
         return self.slug
     
+    def get_absolute_url(self):
+        return "%s#%s" % (self.establishment.get_absolute_url, self.slug)
