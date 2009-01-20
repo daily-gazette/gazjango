ba17cb2733b47c2b797e1df14feaab78ec441167
Dougal Sutherland <dougal@gmail.com>
2009-01-20 01:19:04 +0000

    allow people to mark their books as sold

--- gazjango/books/urls.py
+++ gazjango/books/urls.py
@@ -3,8 +3,9 @@ from gazjango.books.views      import *
 from gazjango.misc.url_helpers import reps
 
 urlpatterns = patterns('',
-    (r'^/?$',                list_books),
-    (r'^new/$',             submit_book),
-    (r'^new/success/$',     book_success),
-    (r'^%(slug)s/$' % reps, book_details),
+    (r'^/?$',                    list_books),
+    (r'^new/$',                  submit_book),
+    (r'^new/success/$',          book_success),
+    (r'^%(slug)s/$' % reps,      book_details),
+    (r'^%(slug)s/sell/$' % reps, mark_as_sold),
 )
--- gazjango/books/views.py
+++ gazjango/books/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers       import reverse
-from django.http      import HttpResponseRedirect
+from django.http      import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template  import RequestContext
 
@@ -9,6 +9,8 @@ from gazjango.books.models      import BookListing
 from gazjango.books.forms       import SubmitBookForm
 from gazjango.comments.models   import PublicComment
 from gazjango.misc.view_helpers import get_user_profile
+
+import datetime
 
 def book_details(request, slug):
     book = get_object_or_404(BookListing, slug=slug)
@@ -25,15 +27,31 @@ def book_details(request, slug):
 
 def list_books(request):
     books = BookListing.published.filter(sold_at=None).order_by('-pub_date')
-    tops, mids, lows = Article.published.get_stories(num_top=1, num_mid=3, num_low=0)
+    profile = get_user_profile(request)
     
     return render_to_response('books/list.html', {
         'books': books,
-        'topstory': tops[0],
-        'stories': mids,
-        'comments': PublicComment.visible.order_by('-time')[:3],
-        'other_books': BookListing.unsold.order_by('-pub_date')[:3]
+        'user_profile': profile,
     }, context_instance=RequestContext(request))
+
+
+def mark_as_sold(request, slug):
+    if request.method in ('POST', 'DELETE'):
+        book = get_object_or_404(BookListing, slug=slug)
+        profile = get_user_profile(request)
+        
+        if profile and profile == book.seller:
+            book.sold_at = datetime.datetime.now()
+            book.save()
+            if request.is_ajax():
+                return HttpResponse('success')
+            else:
+                return HttpResponseRedirect(reverse(list_books))
+    
+    if request.is_ajax():
+        return 'denied'
+    else:
+        raise Http404
 
 
 @login_required
