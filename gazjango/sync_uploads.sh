9a86c226654e8147aee934721e85746bd2564e49
Dougal Sutherland <dougal@gmail.com>
2009-01-30 16:43:03 +0000

    sync_uploads: don't bother with checksumming

--- gazjango/sync_uploads.sh
+++ gazjango/sync_uploads.sh
@@ -3,4 +3,4 @@
 # Synchronize the uploads/ folder from daily.swarthmore.edu to your machine.
 # This will probably take a little while -- as of January 2009, it was 318 MB.
 
-rsync -cvrhz --del --progress dailygazette@daily.swarthmore.edu:gazjango-trunk/gazjango/uploads/ ./uploads/
+rsync -vrhz --del --progress dailygazette@daily.swarthmore.edu:gazjango-trunk/gazjango/uploads/ ./uploads/
