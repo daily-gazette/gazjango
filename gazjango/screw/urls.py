8e4f1a4dd94e4b5db4aa4417901e8874bebee13c
Miles Skorpen <miles@sccs.swarthmore.edu>
2009-02-07 00:04:07 +0000

    Adding a screw app to the site. Hope it doesn't break.

--- gazjango/screw/urls.py
+++ gazjango/screw/urls.py
@@ -3,8 +3,7 @@ from gazjango.screw.views      import *
 from gazjango.misc.url_helpers import reps
 
 urlpatterns = patterns('',
-    (r'^/?$',                    list_screwees),
-    (r'^new/$',                  submit_screwee),
+    (r'^/?$',                    list_screws),
     (r'^new/success/$',          screw_success),
     (r'^%(slug)s/screwed/$' % reps, mark_as_screwed),
 )
