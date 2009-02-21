7087493554641e34a198703906dd59d3a6c17a84
Miles Skorpen <miles@sccs.swarthmore.edu>
2009-02-21 02:59:40 +0000

    Trying to add inline editing for story concepts.

--- gazjango/articles/forms.py
+++ gazjango/articles/forms.py
@@ -15,7 +15,7 @@ class SubmitStoryConcept(forms.ModelForm):
     
 class ConceptSaveForm(forms.Form):
     name = forms.CharField(label = 'Concept',   widget=forms.TextInput(attrs={'size': 64}), required=True)
-    notes= forms.CharField(label = 'Notes',     widget=forms.TextArea( attrs={'cols': 65}), required=True)
+    notes= forms.CharField(label = 'Notes',     widget=forms.Textarea( attrs={'cols': 65}), required=True)
     due  = forms.CharField(label = 'Due Date',  widget=forms.TextInput(attrs={'size': 64})               )
     users= forms.MultipleChoiceField(
         label = 'Assignee',
