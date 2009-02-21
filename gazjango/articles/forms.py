71b9d7c91bbda137aa70688c46cf2606913eff89
Miles Skorpen <miles@sccs.swarthmore.edu>
2009-02-21 03:16:45 +0000

    Trying to add inline editing for story concepts.

--- gazjango/articles/forms.py
+++ gazjango/articles/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.admin import widgets as admin_widgets
+from django.contrib.auth.models import User
 
 from gazjango.articles.models      import StoryConcept
 from gazjango.accounts.models      import UserProfile
@@ -16,7 +17,21 @@ class SubmitStoryConcept(forms.ModelForm):
         fields = ('name','due')
     
 class ConceptSaveForm(forms.Form):
-    name  = forms.CharField(label = 'Concept',   widget=forms.TextInput(attrs={'size': 64}), required=True)
-    notes = forms.CharField(label = 'Notes',     widget=forms.Textarea( attrs={'cols': 65}), required=True)
-    due   = forms.CharField(label = 'Due Date',  widget=forms.TextInput(attrs={'size': 15})               )
-    users = forms.ModelMultipleChoiceField(queryset=User.objects.all())
+    name = forms.CharField(label = 'Concept',   widget=forms.TextInput(attrs={'size': 64}), required=True)
+    notes= forms.CharField(label = 'Notes',     widget=forms.Textarea( attrs={'cols': 65}), required=True)
+    due  = forms.CharField(label = 'Due Date',  widget=forms.TextInput(attrs={'size': 15})               )
+    users = forms.ModelMultipleChoiceField(queryset=User.objects.all(), widget=admin_widgets.FilteredSelectMultiple)
+    
+    
+    users = forms.MultipleChoiceField(
+        
+    )
+    
+    departments = forms.MultipleChoiceField(
+        widget=admin_widgets.FilteredSelectMultiple('departments', False),
+        required=False
+    )
+    self.fields['departments'].choices = [
+        (tag.pk, tag.longest_name())
+        for tag in departments_taggroup().tags.all()
+    ]
