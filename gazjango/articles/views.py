a20d8167e15f7ca7925779a459e58cd278ba96a5
Miles Skorpen <miles@sccs.swarthmore.edu>
2009-02-21 03:55:51 +0000

    Setting up the save page to accept Get requests

--- gazjango/articles/views.py
+++ gazjango/articles/views.py
@@ -261,6 +261,8 @@ def concept_save_page(request, template="staff/submit.html"):
             return HttpResponseRedirect('/staff/')
     elif request.GET.has_key('name'):
         name = request.GET['name']
+        due  = ""
+        notes= ""
         try:
             concept = StoryConcepts.objects.get(name=name)
             notes = concept.notes
