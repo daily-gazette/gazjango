d7d428f4cec167a1a3277610d3812e4834f71920
Miles Skorpen <miles@sccs.swarthmore.edu>
2009-02-22 06:37:28 +0000

    Fixing up the JS? Might solve?

--- gazjango/articles/forms.py
+++ gazjango/articles/forms.py
@@ -19,4 +19,4 @@ class SubmitStoryConcept(forms.ModelForm):
 class ConceptSaveForm(forms.Form):        
     name  = forms.CharField(label = 'Concept',   widget=forms.TextInput(attrs={'size': 20}), required=True)
     due   = forms.CharField(label = 'Due Date',  widget=forms.TextInput(attrs={'size': 15}),   required=False)
-    users = forms.ModelMultipleChoiceField(label = 'Users', queryset=User.objects.all(), widget=admin_widgets.FilteredSelectMultiple('Users', False), required=False)
+    users = forms.ModelMultipleChoiceField(label = 'Users', queryset=User.objects.all(), widget=admin_widgets.FilteredSelect('Users', False), required=False)
