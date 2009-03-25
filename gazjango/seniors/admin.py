3fece3903f53eacdaa6feef3bcfb7982ae5264d7
Miles Skorpen <miles@sccs.swarthmore.edu>
2009-03-25 02:17:02 +0000

    removing seniors

--- gazjango/seniors/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from gazjango.seniors.models import SeniorListing
-
-class SeniorAdmin(admin.ModelAdmin):
-    list_display = ('senior', 'city', 'state', 'position')
-    list_filter = ('position',)
-    date_hierarchy = 'pub_date'
-    prepopulated_fields = { 'slug': ('senior',) }
-admin.site.register(SeniorListing, SeniorAdmin)
