0d25a3441190ca0b65536462b0eabf34a78fadd4
The Daily Gazette <dailygazette@swarthmore.edu>
2009-07-05 23:40:21 +0000

    link change

--- gazjango/scrapers/manual_links.py
+++ gazjango/scrapers/manual_links.py
@@ -5,15 +5,15 @@ manual_links = [
 	'<a href="http://www.istudentloan.com/">Student Loan</a>',
 	'<a href="http://www.elearners.com/colleges/">Accredited Online Schools</a>',
     '<a href="http://www.studentcreditcards.com/">student credit cards</a><br> www.studentcreditcards.com/ - student credit cards.com features informative weekly articles on credit as well as a studentcredit card comparison.',
-    '<a href="http://ww.arcadekill.com">Free Flash Arcade Games</a>',
+    '<a href="http://www.arcadekill.com">Free Flash Arcade Games</a>',
     '<a href="http://repairmycreditnow.com/Credit_Repair_Advice.htm">Credit Repair</a>',
+'<a href="http://www.creditscorequick.com">Free Credit Report</a>',
 ]
 
 lca_links = [
     ('Rosetta Coupon', "http://www.couponcactus.com/coupons/Rosetta_Stone/"),
     ('college loans', "http://www.thinkfinancial.com/"),
     ('acai', "http://www.acairoots.com"),
-    ('Last Minute Hotel', "http://www.hotel-erb.de"),
     ('office furniture', "http://www.officefurniture2go.com/"),
     ('Custom Essays', "http://www.essaybay.com"),
     ('Limo, Town Car, Party Bus', "http://www.limolinkexchange.com/"),
