52520baa2104416b899157629ffb7d0c6689df07
The Daily Gazette <dailygazette@swarthmore.edu>
2009-08-21 20:59:19 +0000

    import Http404, which is used here

--- gazjango/announcements/views.py
+++ gazjango/announcements/views.py
@@ -1,4 +1,4 @@
-from django.http                   import HttpResponseRedirect
+from django.http                   import HttpResponseRedirect, Http404
 from django.template               import RequestContext
 from django.core.urlresolvers      import reverse
 from django.shortcuts              import render_to_response, get_object_or_404
