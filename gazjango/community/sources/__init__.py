dedcd9938dab290c462dc4be6a42ba873eb174eb
Dougal Sutherland <dougal@mini-server.(none)>
2009-12-30 19:43:32 -0500

    fix the import trickery in community.sources

--- gazjango/community/sources/__init__.py
+++ gazjango/community/sources/__init__.py
@@ -20,8 +20,8 @@ def import_source_modules(source_list=settings.AGRO_SETTINGS['source_list'], cla
     for source in source_list:
         log.debug('trying to load %s' % source)
         try:
-            # CHANGED: make sure this works :)
-            sources.append(__import__("community.sources.%s" % source))
+            sources.append(__import__("community.sources.%s" % source, 
+                                      fromlist=['%s%s' % (source, class_name)]))
         except ImportError as e:
             log.error('unable to load %s: %s', source, e)
     return sources
