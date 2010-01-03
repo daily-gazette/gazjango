ac8b9134e76594f1f9d093ae834603ce24f3b04b
Dougal Sutherland <dougal@gmail.com>
2010-01-02 21:02:00 -0400

    clean up community models/managers

--- gazjango/community/managers.py
+++ gazjango/community/managers.py
@@ -1,26 +1,57 @@
 from django.db import models
 
 class EntryManager(models.Manager):
-    def get_of_type_for_user(self, type_str_or_list, username_or_list):
-        obj_list = self.get_of_type(type_str_or_list)
-        if obj_list:
-            return self.get_for_user(username_or_list, obj_list)
-        return obj_list
+    def get_of_type_for_user(self, types, usernames):
+        "Get entries of type `types` from the users `usernames`."
+        return self.get_for_user(usernames, base=self.get_of_type(types))
+    
+    def get_of_type(self, types, base=None):
+        """
+        If `types` is a list/tuple, return entries of any of those types; otherwise, return
+        entries of type `types`, or all of them if `types` is the special value "entries".
+        
+        Use `base` as the optional base for filtering.
+        """
+        if not base:
+            base = self.all()
+        
+        if types == "entries":
+            return base
+        elif isinstance(types, (list, tuple,)):
+            return base.filter(source_type__in=types)
+        else:
+            return base.filter(source_type=types)
+    
+    def get_for_user(self, usernames, base=None):
+        "Get entries from the username or list of usernames `usernames`."
+        if not base:
+            base = self.all()
+        
+        if isinstance(usernames, (list, tuple)):
+            return base.filter(owner_user__in=usernames)
+        elif isinstance(usernames, basestring):
+            return base.filter(owner_user=usernames)
+        else:
+            return base
+    
 
-    def get_of_type(self, type_str_or_list, obj_list=None):
-        if not obj_list:
-            obj_list = super(EntryManager, self).all()
-        if isinstance(type_str_or_list, (list, tuple,)):
-            return obj_list.filter(source_type__in=type_str_or_list)
-        if type_str_or_list == 'entries':
-            return obj_list
-        return obj_list.filter(source_type=type_str_or_list)
-
-    def get_for_user(self, username_or_list, obj_list=None):
-        if not obj_list:
-            obj_list = super(EntryManager, self).all()
-        if isinstance(username_or_list, list):
-            return obj_list.filter(owner_user__in=username_or_list)
-        elif isinstance(username_or_list, (str, unicode)):
-            return obj_list.filter(owner_user=username_or_list)
-        return False
+class PublishedEntryManager(EntryManager):
+    "A custom manager for entries, returning only published articles."
+    
+    def get_query_set(self):
+        orig = super(PublishedEntryManager, self).get_query_set()
+        return orig.filter(status__gte=2, timestamp__lte=datetime.datetime.now())
+    
+    def get_entries(self, base=None, num=10, exclusion=None, category=None):
+        entries = (base or self).order_by('-timestamp')
+        if category:
+            entries = entries.filter(source_type=category)
+        if exclusion:
+            entries = entries.filter(source_type=exclusion)
+        return list(entries[:num])
+    
+    def get_photos(self, base=None, num=3):
+        return list((base or self).order_by('-timestamp').filter(source_type="flickrphoto")[:num])
+    
+    def get_tweets(self, base=None, num=3):
+        return list((base or self).order_by('-timestamp').filter(source_type="tweet")[:num])
--- gazjango/community/models.py
+++ gazjango/community/models.py
@@ -1,75 +1,49 @@
 import datetime
 
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from django.db.models import permalink
-from django.contrib.auth.models import User
 
-from gazjango.community.managers import EntryManager
+from gazjango.community.managers import EntryManager, PublishedEntryManager
 from gazjango.community.sources import import_source_modules
-
-class PublishedEntryManager(models.Manager):
-    "A custom manager for entries, returning only published articles."
-    
-    def get_query_set(self):
-        orig = super(PublishedEntryManager, self).get_query_set()
-        return orig.filter(status__gte=2, timestamp__lte=datetime.datetime.now())
-    
-    def get_entries(self, base=None, num=10, exclusion=False,category=False):
-        base = base or self
-        if category and exclusion:
-            entries = list(base.order_by('-timestamp').filter(source_type=category).exclude(source_type=exclusion)[:num])
-        elif category:
-            entries = list(base.order_by('-timestamp').filter(source_type=category)[:num])
-        elif exclusion:
-            entries = list(base.order_by('-timestamp').exclude(source_type=exclusion)[:num])
-        else:
-            entries = list(base.order_by('-timestamp')[:num])
-        return entries    
-    
-    def get_photos(self,base=None,num=3):
-        base = base or self
-        entries = list(base.order_by('-timestamp').filter(source_type="flickrphoto")[:num])
-        return entries
-        
-    def get_tweets(self,base=None,num=3):
-        base = base or self
-        entries = list(base.order_by('-timestamp').filter(source_type="tweet")[:num])
-        return entries
 
 class Entry(models.Model):
     STATUS_CHOICES = (
-      (1, 'Draft'),
-      (2, 'Public'),
+        (1, 'Draft'),
+        (2, 'Public'),
     )
     
     # For All
-    title           = models.CharField(max_length=200, help_text="Main text, or Title, of your entry.", blank=True)
-    timestamp       = models.DateTimeField(help_text="Timestamp for your entry. This is how we pull items out of the DB.", blank=True)
-    description     = models.TextField(help_text="Description, or subtext, of your entry.", blank=True)
-    status          = models.IntegerField(_('status'), choices=STATUS_CHOICES, default=2)
+    title = models.CharField(max_length=200, blank=True,
+        help_text="Main text, or Title, of your entry.")
+    timestamp = models.DateTimeField(blank=True,
+        help_text="Timestamp for your entry. This is how we pull items out of the DB.")
+    description = models.TextField(blank=True,
+        help_text="Description, or subtext, of your entry.")
+    status = models.IntegerField('status', choices=STATUS_CHOICES, default=2)
     
     # For Services
-    owner_user      = models.CharField(max_length=100, help_text="Here we store the username used for the webservice, for this entry.", blank=True)
-    url             = models.URLField(verify_exists=False, help_text="URL back to the original item.", blank=True)
-    source_type     = models.CharField(max_length=100, help_text="Type of entry.", blank=True, default="post")
-
-    objects         = EntryManager()
-    published       = PublishedEntryManager()
+    owner_user = models.CharField(max_length=100, blank=True,
+        help_text="Here we store the username used for the webservice, for this entry.")
+    url = models.URLField(verify_exists=False, blank=True,
+        help_text="URL for the original item.")
+    source_type = models.CharField(max_length=100, blank=True, default="post",
+        help_text="Type of entry.")
+    
+    objects   = EntryManager()
+    published = PublishedEntryManager()
     
     class Meta:
         get_latest_by = 'timestamp'
         ordering = ['-timestamp']
-        app_label = "community"
         verbose_name_plural = "entries"
         unique_together = [('title', 'timestamp', 'source_type'),]
-
+    
     def __unicode__(self):
         return u"%s" % self.title
-
+    
     def __cmp__(self, other_entry):
         return cmp(self.timestamp, other_entry.timestamp)
-
+    
     @property
     def object(self):
         return getattr(self, self.source_type)
