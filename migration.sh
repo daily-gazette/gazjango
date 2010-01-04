49e66c66ebdbb3f126b41adf06643f17a170353a
Dougal Sutherland <dougal@gmail.com>
2010-01-03 20:54:11 -0400

    add the db migration script

---
+++ migration.sh
@@ -0,0 +1,49 @@
+##### migration: v2009 => v2010
+
+# convert to staff_state column for positions
+./manage.py dbshell <<'SQL'
+BEGIN;
+ALTER TABLE `accounts_position` ADD COLUMN 
+            `staff_state` varchar(1) NOT NULL DEFAULT 'n';
+UPDATE `accounts_position` SET `staff_state`='n';
+UPDATE `accounts_position` SET `staff_state`='e' WHERE `is_editor`=1;
+UPDATE `accounts_position` SET `staff_state`='s' WHERE `name` 
+        IN ('Staff Reporter', 'Staff Photographer');
+UPDATE `accounts_position` SET `staff_state`='x' WHERE `name` 
+        IN ('Editor Emeritus', 'Ex-Staff');
+ALTER TABLE `accounts_position` DROP COLUMN `is_editor`;
+COMMIT;
+SQL
+
+# add num_full column for issues
+./manage.py dbshell <<'SQL'
+BEGIN;
+ALTER TABLE `issues_issue` ADD COLUMN 
+            `num_full` integer NOT NULL DEFAULT 3
+            AFTER `id`;
+COMMIT;
+SQL
+
+# add the community app
+./manage.py sqlall community | ./manage.py dbshell
+
+# add posters
+./manage.py dbshell <<'SQL'
+BEGIN;
+CREATE TABLE `announcements_poster` (
+    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    `title` varchar(100) NOT NULL,
+    `poster_id` integer,
+    `sponsor_url` varchar(200) NOT NULL,
+    `sponsor_user_id` integer NOT NULL,
+    `date_start` date NOT NULL,
+    `date_end` date NOT NULL,
+    `is_published` bool NOT NULL,
+    `related_event_id` integer NOT NULL
+)
+;
+ALTER TABLE `announcements_poster` ADD CONSTRAINT `poster_id_refs_id_edc8472e` FOREIGN KEY (`poster_id`) REFERENCES `media_imagefile` (`id`);
+ALTER TABLE `announcements_poster` ADD CONSTRAINT `sponsor_user_id_refs_id_d23acd80` FOREIGN KEY (`sponsor_user_id`) REFERENCES `accounts_userprofile` (`id`);
+ALTER TABLE `announcements_poster` ADD CONSTRAINT `related_event_id_refs_id_344f307` FOREIGN KEY (`related_event_id`) REFERENCES `announcements_announcement` (`id`);
+COMMIT;
+SQL
