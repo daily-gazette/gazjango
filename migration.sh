aa667145c71c433280dfc4b77b7244b6ce231860
Dougal Sutherland <dougal@gmail.com>
2010-01-12 16:06:44 -0400

    migration.sh: switch to using syncdb for everything that it can do

--- migration.sh
+++ migration.sh
@@ -2,6 +2,9 @@
 
 # ALTER DATABASE gazette CHARACTER SET 'utf8';
 
+# add missing apps/tables
+./manage.py syncdb
+
 # convert to staff_state column for positions
 ./manage.py dbshell <<'SQL'
 BEGIN;
@@ -26,37 +29,13 @@ ALTER TABLE `issues_issue` ADD COLUMN
 COMMIT;
 SQL
 
-# add the community app
-./manage.py sqlall community | ./manage.py dbshell
-
-# add the ads app
-./manage.py sqlall ads | ./manage.py dbshell
+# add our livecustomer ads
 ./manage.py shell <<'ADS'
 from interactive_load import *
 TextLinkAd.objects.create(source='c', link='http://www.acairoots.com', text='acai')
 TextLinkAd.objects.create(source='c', link='http://www.r4-ds-card.ca', text='r4')
+TextLinkAd.objects.create(source='c', link='http://www.framesdirect.com/sunglasses/', text='sunglasses')
 ADS
 
-# add posters
-./manage.py dbshell <<'SQL'
-BEGIN;
-CREATE TABLE `announcements_poster` (
-    `id` integer AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    `title` varchar(100) NOT NULL,
-    `poster_id` integer,
-    `sponsor_url` varchar(200) NOT NULL,
-    `sponsor_user_id` integer NOT NULL,
-    `date_start` date NOT NULL,
-    `date_end` date NOT NULL,
-    `is_published` bool NOT NULL,
-    `related_event_id` integer NOT NULL
-)
-;
-ALTER TABLE `announcements_poster` ADD CONSTRAINT `poster_id_refs_id_edc8472e` FOREIGN KEY (`poster_id`) REFERENCES `media_imagefile` (`id`);
-ALTER TABLE `announcements_poster` ADD CONSTRAINT `sponsor_user_id_refs_id_d23acd80` FOREIGN KEY (`sponsor_user_id`) REFERENCES `accounts_userprofile` (`id`);
-ALTER TABLE `announcements_poster` ADD CONSTRAINT `related_event_id_refs_id_344f307` FOREIGN KEY (`related_event_id`) REFERENCES `announcements_announcement` (`id`);
-COMMIT;
-SQL
-
-# makes sure that admin permissions are right, etc
+# make sure that admin permissions are right, etc
 ./manage.py syncdb
