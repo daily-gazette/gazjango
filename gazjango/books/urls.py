9e224869139e2ab6b8b861d3a441ef412c2c7e38
Dougal Sutherland <dougal@gmail.com>
2010-01-18 16:33:50 -0500

    add a url shortcut for the books list

--- gazjango/books/urls.py
+++ gazjango/books/urls.py
@@ -3,7 +3,7 @@ from gazjango.books.views      import *
 from gazjango.misc.url_helpers import reps
 
 urlpatterns = patterns('',
-    (r'^/?$',                    list_books),
+    (r'^/?$',                    list_books, {}, 'books'),
     (r'^new/$',                  submit_book),
     (r'^new/success/$',          book_success),
     (r'^%(slug)s/$' % reps,      book_details),
