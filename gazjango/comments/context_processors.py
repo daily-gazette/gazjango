b1e7fc51d7985b4ed764551bda62b80784269328
Dougal Sutherland <dougal@gmail.com>
2010-01-23 20:22:51 -0500

    cache the recent popular comments thing

--- gazjango/comments/context_processors.py
+++ gazjango/comments/context_processors.py
@@ -1,12 +1,14 @@
 from gazjango.comments.models import PublicComment
+from gazjango.misc.helpers import cache
 import heapq
 
 NUM_CONSIDERED = 50
-NUM_RETURNED = 6
+NUM_RETURNED = 5
 
-def popular_comments(request):
+@cache(60 * 60)
+def popular_comments(request, range=NUM_CONSIDERED, num=NUM_RETURNED):
     return {
-        'popular_comments': heapq.nlargest(NUM_RETURNED,
-            PublicComment.objects.order_by('-time')[:NUM_CONSIDERED],
+        'popular_comments': heapq.nlargest(num,
+            PublicComment.objects.order_by('-time')[:range],
             key=PublicComment.num_upvotes)
     }
