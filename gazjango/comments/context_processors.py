6795489843d2c3726f910b2d905be1455bc74855
Dougal Sutherland <dougal@gmail.com>
2010-01-23 23:46:25 -0500

    switch to 6 comments in the footer

--- gazjango/comments/context_processors.py
+++ gazjango/comments/context_processors.py
@@ -3,7 +3,7 @@ from gazjango.misc.helpers import cache
 import heapq
 
 NUM_CONSIDERED = 50
-NUM_RETURNED = 5
+NUM_RETURNED = 6
 
 @cache(60 * 60)
 def popular_comments(request, range=NUM_CONSIDERED, num=NUM_RETURNED):
