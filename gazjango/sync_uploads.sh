6a6ee277e8577b93306ab2973342a0faea2c5f52
Dougal Sutherland <dougal@gmail.com>
2010-01-23 22:28:51 -0500

    sync_uploads: don't bother with checksums, and ignore the resized directory

--- gazjango/sync_uploads.sh
+++ gazjango/sync_uploads.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # Synchronize the uploads/ folder from daily.swarthmore.edu to your machine.
-# This will probably take a little while -- as of January 2009, it was 318 MB.
 
-rsync -cvrhz --del --progress dailygazette@daily.swarthmore.edu:gazjango-trunk/gazjango/uploads/ ./uploads/
+# This will probably take a little while -- as of January 2010, it was 600MB in
+# about 4,000 files.
+
+rsync -vrhz --exclude resized --del --progress dailygazette@daily.swarthmore.edu:gazjango-trunk/gazjango/uploads/ ./uploads/
