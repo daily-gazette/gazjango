f272d6abbed4206a04df904f66ae2e01bfe269e2
Dougal Sutherland <dougal@gmail.com>
2010-03-09 23:21:39 -0500

    add a housing link

--- gazjango/housing/urls.py
+++ gazjango/housing/urls.py
@@ -3,6 +3,6 @@ from gazjango.housing.views      import *
 from gazjango.misc.url_helpers import reps
 
 urlpatterns = patterns('',
-    (r'^/?$',                    list_housing),
-    (r'^new/success/$',          senior_success),
+    (r'^/?$',            list_housing,   {}, 'housing'),
+    (r'^new/success/$',  senior_success),
 )
