73ca8c59a112f81f02c047b2c321ddee1add3a7a
Dougal Sutherland <dougal@gmail.com>
2010-03-14 01:37:35 -0500

    fix up the handle_file_upload function

--- gazjango/misc/files.py
+++ gazjango/misc/files.py
@@ -1,15 +1,22 @@
-import os
+import os, os.path
 from django.conf import settings
 
-def handle_file_upload(f, bucket_name):
+def handle_file_upload(f, folder_name, return_full_path=False):
     """Save an uploaded file into the appropriate location."""
     
-    directory = os.path.join(settings.MEDIA_ROOT, bucket_name)
+    directory = os.path.join(settings.MEDIA_ROOT, folder_name)
     if not os.path.isdir(directory):
         os.makedirs(directory)
-    path = os.path.join(directory, f.name)
     
-    dest = open(path, 'wb+')
+    name = f.name
+    while os.path.isfile(os.path.join(directory, name)):
+        name = '_' + name
+    
+    dest = open(os.path.join(directory, name), 'wb+')
     for chunk in f.chunks():
         dest.write(chunk)
-    return path
+
+    if return_full_path:
+        return os.path.join(directory, name)
+    else:
+        return os.path.join(folder_name, name)
