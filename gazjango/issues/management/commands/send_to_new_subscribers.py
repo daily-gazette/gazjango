70c751046a12cbb18030664402644778fe770a73
Dougal Sutherland <dougal@gmail.com>
2010-10-19 02:37:18 -0400

    command for sending to new subscribers only

---
+++ gazjango/issues/management/commands/send_to_new_subscribers.py
@@ -0,0 +1,5 @@
+from gazjango.issues.management.commands import send_issue_directly
+from gazjango.subscriptions.models import Subscriber
+
+class Command(send_issue_directly.Command):
+    subscriber_base = Subscriber.issues.filter(last_sent=None)
