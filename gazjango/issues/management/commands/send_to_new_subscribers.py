b77ec268be5655a6af4ce4a6068dcdb12e212b1d
Dougal Sutherland <dougal@gmail.com>
2010-10-19 02:46:38 -0400

    last_sent defaults to '', not None

--- gazjango/issues/management/commands/send_to_new_subscribers.py
+++ gazjango/issues/management/commands/send_to_new_subscribers.py
@@ -2,4 +2,4 @@ from gazjango.issues.management.commands import send_issue_directly
 from gazjango.subscriptions.models import Subscriber
 
 class Command(send_issue_directly.Command):
-    subscriber_base = Subscriber.issues.filter(last_sent=None)
+    subscriber_base = Subscriber.issues.filter(last_sent='')
