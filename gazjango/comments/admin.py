435c0a8be3c6f43ae7003c8aa9f747859cb9c7f9
Dougal Sutherland <dougal@gmail.com>
2010-12-01 03:45:54 -0500

    add a comment approve action

--- gazjango/comments/admin.py
+++ gazjango/comments/admin.py
@@ -12,7 +12,7 @@ class PublicCommentAdmin(admin.ModelAdmin):
     search_fields = ('text', 'article__headline', 'name', 'email', 
                      'user__user__first_name', 'user__user__last_name', 'user__user__email')
 
-    actions = ['mark_as_spam']
+    actions = ['mark_as_spam', 'approve']
 
     def mark_as_spam(self, request, queryset):
         counter = 0
@@ -21,5 +21,14 @@ class PublicCommentAdmin(admin.ModelAdmin):
             counter += 1
         self.message_user(request, "%s comments were successfully spammed" % counter)
     mark_as_spam.short_description = "Mark selected comments as spam (deletes them too)"
+
+    def approve(self, request, queryset):
+        rows_updated = queryset.update(is_approved=True)
+        if rows_updated == 1:
+            message_bit = "1 comment was"
+        else:
+            message_bit = "%s comments were" % rows_updated
+        self.message_user(request, message_bit + " were successfully approved.")
+    mark_as_spam.short_description = "Approve selected comments"
 
 admin.site.register(PublicComment, PublicCommentAdmin)
