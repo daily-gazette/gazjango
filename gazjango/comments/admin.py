ab749ca09819b8bcbbc4b80bf926965fa0a5ebbd
Dougal Sutherland <dougal@gmail.com>
2010-12-01 03:42:56 -0500

    add a "mark_as_spam" admin action for comments

--- gazjango/comments/admin.py
+++ gazjango/comments/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from gazjango.comments.models import PublicComment
 
+
+
 class PublicCommentAdmin(admin.ModelAdmin):
     date_hierarchy = 'time'
     list_display = ('article_name', 'number', 'display_name', 'user_or_email',
@@ -9,4 +11,15 @@ class PublicCommentAdmin(admin.ModelAdmin):
     list_filter = ('is_approved', 'superhidden')
     search_fields = ('text', 'article__headline', 'name', 'email', 
                      'user__user__first_name', 'user__user__last_name', 'user__user__email')
+
+    actions = ['mark_as_spam']
+
+    def mark_as_spam(self, request, queryset):
+        counter = 0
+        for comment in queryset:
+            comment.mark_as_spam()
+            counter += 1
+        self.message_user(request, "%s comments were successfully spammed" % counter)
+    mark_as_spam.short_description = "Mark selected comments as spam (deletes them too)"
+
 admin.site.register(PublicComment, PublicCommentAdmin)
